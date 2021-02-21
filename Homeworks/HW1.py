2dffd604574a33ea1e1ed1302698067bbc3c1d81
erenne314 <chuckrecordings@outlook.com>
2021-02-21 23:51:15 +0300

    dosyalar yüklendi / açıklama / iyi akşamlar

---
+++ Homeworks/HW1.py
@@ -0,0 +1,7 @@
+print('açıklama satırıma bakabilirsiniz')
+print('Repo name --> GlobalAIHubPythonCourse <-- )
+##Merhaba ben Eren ödevleri kontrol edenlere kolay gelsin demek istiyorum.
+##Kurs için teşekkürler olabildiğince dinlemeye çalıştım. Emeğinize sağlık tekrar teşekkürler.
+##Ödevleri yüklememizi istenilen Repo'nun adının "GlobalAIHubPythonCourse" olması istenmişti bizden.
+##İçgüdülerimde haklıysam çoğu kişi bu kısmı gözden kaçırdı ya da ben öyle umuyorum.
+##Eğer bu Repo koşulunu sağlayan yüzde birlik bir çoğunluk yoksa ufak bir şans istiyorum.
